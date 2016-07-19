5bb1901604b29add21e799c892164dcfd20759d6
Fabio Massaretto <fmassaretto@gmail.com>
2016-07-19 02:41:59 -0300

    Adding and removing some code

--- dbconnector/DBconnector.java
+++ dbconnector/DBconnector.java
@@ -14,36 +14,40 @@ import java.sql.SQLException;
  * @author Fabio
  */
 public class DBconnector {
+    public Connection connection = null;
+    
+    public void conexao(){
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
 
-    public static void DBconnector() {
+                    try {
+                            Class.forName("com.mysql.jdbc.Driver");
+                    } catch (ClassNotFoundException e) {
+                            System.out.println("Where is your MySQL JDBC Driver?");
+                            e.printStackTrace();
+                            return;
+                    }
 
-	System.out.println("-------- MySQL JDBC Connection Testing ------------");
+                    System.out.println("MySQL JDBC Driver Registered!");
+                    
 
-                try {
-                        Class.forName("com.mysql.jdbc.Driver");
-                } catch (ClassNotFoundException e) {
-                        System.out.println("Where is your MySQL JDBC Driver?");
-                        e.printStackTrace();
-                        return;
-                }
+                    try {
+                            connection = DriverManager
+                            .getConnection("jdbc:mysql://localhost:3306/projetozikadb","root", "lmi56n");
 
-                System.out.println("MySQL JDBC Driver Registered!");
-                Connection connection = null;
+                    } catch (SQLException e) {
+                            System.out.println("Connection Failed! Check output console");
+                            e.printStackTrace();
+                            return;
+                    }
 
-                try {
-                        connection = DriverManager
-                        .getConnection("jdbc:mysql://localhost:3306/projetozikadb","root", "lmi56n");
-
-                } catch (SQLException e) {
-                        System.out.println("Connection Failed! Check output console");
-                        e.printStackTrace();
-                        return;
-                }
-
-                if (connection != null) {
-                        System.out.println("You made it, take control your database now!");
-                } else {
-                        System.out.println("Failed to make connection!");
-                }
+                    if (connection != null) {
+                            System.out.println("You made it, take control your database now!");
+                    } else {
+                            System.out.println("Failed to make connection!");
+                    }
+    }
+    public void DBconnector() {
+        
+	this.conexao();
   }
 }
